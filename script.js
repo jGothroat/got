279a4af5f41e3d5966788427af6b9d4b678d2aca
jGothroat <dammhjxxc@gmail.com>
2025-01-26 01:49:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Age verification functionality
+document.getElementById('enter-site').addEventListener('click', function () {
+  document.getElementById('age-verification').style.display = 'none';
+  document.getElementById('main-content').style.display = 'block';
+});
